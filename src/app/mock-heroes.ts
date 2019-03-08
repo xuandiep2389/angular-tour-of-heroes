ba95374e7d696953f8bc0069d80eac04337c500f
vu linh <xuandiep2389@gmail.com>
2019-03-08 10:57:35 +0700

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 2 , name: 'Captain America'},
+  {id: 3 , name: 'Hulk'},
+  {id: 4 , name: 'Thor'},
+  {id: 5 , name: 'Iron man'},
+  {id: 6 , name: 'Aqua man'},
+  {id: 7 , name: 'Captain America'},
+  {id: 8 , name: 'Captain America'},
+  {id: 9 , name: 'Captain America'},
+  {id: 10 , name: 'Captain America'},
+  {id: 11 , name: 'Captain America'}
+];
